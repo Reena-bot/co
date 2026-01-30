58cbb6b5424e1cc96e79c2bbe290033b5af36699
Reena Kumari <rinapandey585@gmail.com>
2026-01-30 17:28:02 +0530

    Create submission.php

---
+++ submission.php
@@ -0,0 +1,33 @@
+<?
+echo"php is working";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Successful</title>
+</head>
+<style>
+    body
+    {
+        font-family: Arial, sans-serif;
+        background: #c47b40ff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        flex-direction: column;
+        text-shadow: 1px 1px 2px black;
+        -webkit-text-fill-color: black;
+    }
+    h1
+    {
+        font-size: 50px;
+        margin-bottom: 20px;
+    }
+
+</style>
+<body>
+    <h1>Registration Successful!</h1>
+    <p>Thank you for registering. You can now <a href="login.php">login here</a>.</p>
+</body>
+</html>
